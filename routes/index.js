f83f569c579baf3537136ca8b4f6fc92562d2ee8
Ashis <developer.ashis99@gmail.com>
2023-12-23 16:47:44 +0530

    Optimised Code!

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,35 @@
-import express from 'express'
-import { register,loginMiddleware, logoutUser } from '../controllers/auth_controller.js';
-import { redirectFunc, urlShortener } from '../controllers/url_controller.js';
-import passport from 'passport';
-
+import express from "express";
+import {
+  register,
+  loginMiddleware,
+  logoutUser,
+} from "../controllers/auth_controller.js";
+import { redirectFunc, urlShortener } from "../controllers/url_controller.js";
+import passport from "passport";
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+    return res.status(404).json({
+      message: "Welcome to the URL Shortening API!",
+      instructions: "Use Postman or Thunderclient to interact with this API!",
+    });
+  });
+  
 
 // For User Registration
-router.post('/register', register)
+router.post("/register", register);
 
 // For User Sign In
-router.post('/login', loginMiddleware)
+router.post("/login", loginMiddleware);
 
 // for Logout
-router.get('/logout', logoutUser)
+router.get("/logout", logoutUser);
 
 // For Url Shortening
-router.post('/urlShort', urlShortener)
+router.post("/urlShort", urlShortener);
 
 // Redirection to Orginal Url for Shortened Links
-router.get('/shorty/:id', redirectFunc)
+router.get("/shorty/:id", redirectFunc);
 
 export default router;
